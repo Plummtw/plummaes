fc6761a53ad7ef1145360f35c8edc74afd52d0ed
Plummtw <sailormoontw@gmail.com>
2022-03-27 17:12:32 +0800

    0.1.3

--- README.md
+++ README.md
@@ -7,9 +7,10 @@ usage:
 - plummaes encrypt <input> <output> <keyfile>
 - plummaes decrypt <input> <output> <keyfile>
 
-Note : keyfile must be 64 bytes
+Note : keyfile must be 64 bytes or more
 
 version:
 - v0.1.0 : AES256(no iv) + key xor 
 - v0.1.1 : Add Gzip Compress and File Header "Plumm 1.1"
 - v0.1.2 : Allow encrypt input to be Plain Text (When File Not Found)
+- v0.1.3 : Allow keyfile to be over 64 bytes (only first 64 bytes are used)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::fs::File;
 use std::env;
 use std::io::Read;
 use std::io::Write;
@@ -12,9 +13,22 @@ use flate2::read::GzDecoder;
 use flate2::Compression;
 
 fn read_key_file(file: &str) -> Vec<u8> {
-    match std::fs::read(file) {
-        Ok(bytes) if bytes.len() == 64 => bytes,
-        Ok(_) => panic!("Key File Length Error"),
+    // match std::fs::read(file) {
+    //     Ok(bytes) if bytes.len() >= 64 => bytes,
+    //     Ok(_) => panic!("Key File Length Error"),
+    //     Err(_) => panic!("Error reading key file")
+    // }
+    match File::options().read(true).open(file) {
+        Ok(mut file) => {
+            if file.metadata().unwrap().len() < 64 {
+                panic!("Key File Length Error");
+            }
+
+            let mut buffer = [0u8; 64];
+            // read exactly 10 bytes
+            file.read_exact(&mut buffer).unwrap();
+            buffer.to_vec()
+        },
         Err(_) => panic!("Error reading key file")
     }
 }
